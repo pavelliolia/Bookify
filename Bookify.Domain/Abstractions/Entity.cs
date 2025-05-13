46e4d3297b3e0457b68861cf0a7bfa9d5987eb52
pavelliolia <pavel.liolia.official@gmail.com>
2025-05-13 19:54:44 +0300

    [14-16]: add User entity and domain events (MediatR)

--- Bookify.Domain/Abstractions/Entity.cs
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -2,10 +2,18 @@ namespace Bookify.Domain.Abstractions;
 
 public abstract class Entity
 {
+    private readonly List<IDomainEvent> _domainEvents = new();
+
     protected Entity(Guid id)
     {
         Id = id;
     }
 
     public Guid Id { get; init; }
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents() => _domainEvents;
+
+    public void ClearDomainEvents() => _domainEvents.Clear();
+
+    protected void RaisDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
 }
