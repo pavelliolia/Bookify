1ffa2642fe118a9d8ee4256c6a8df877af0e6915
pavelliolia <pavel.liolia.official@gmail.com>
2025-06-18 11:55:41 +0300

    [40]: automatically apply entity type configs by scanning the assembly

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -5,4 +5,9 @@ namespace Bookify.Infrastructure;
 
 public sealed class ApplicationDbContext(DbContextOptions options) : DbContext(options), IUnitOfWork
 {
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
 }
