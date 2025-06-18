b4f97129f468a08df54c5f20a523bf073d03c626
pavelliolia <pavel.liolia.official@gmail.com>
2025-06-18 12:33:04 +0300

    [42]: implement sql connection factory

--- Bookify.Infrastructure/DependencyInjection.cs
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -1,12 +1,15 @@
 using Bookify.Application.Abstrastions.Clock;
+using Bookify.Application.Abstrastions.Data;
 using Bookify.Application.Abstrastions.Email;
 using Bookify.Domain.Abstractions;
 using Bookify.Domain.Apartments;
 using Bookify.Domain.Bookings;
 using Bookify.Domain.Users;
 using Bookify.Infrastructure.Clock;
+using Bookify.Infrastructure.Data;
 using Bookify.Infrastructure.Email;
 using Bookify.Infrastructure.Repositories;
+using Dapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,7 +24,7 @@ public static class DependencyInjection
         services.AddSingleton<IEmailService, EmailService>();
 
         var connectionString = configuration.GetConnectionString("Database")
-            ?? throw new ArgumentNullException(nameof(configuration));
+                               ?? throw new ArgumentNullException(nameof(configuration));
 
         services.AddDbContext<ApplicationDbContext>(options =>
         {
@@ -32,6 +35,10 @@ public static class DependencyInjection
         services.AddScoped<IApartmentRepository, ApartmentRepository>();
         services.AddScoped<IBookingRepository, BookingRepository>();
         services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+        services.AddSingleton<ISqlConnectionFactory>(_ => new SqlConnectionFactory(connectionString));
+
+        SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
 
         return services;
     }
