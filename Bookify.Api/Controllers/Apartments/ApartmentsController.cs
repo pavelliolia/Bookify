cd7e1ca85ece9dafb660b9e094952d58880189d6
pavelliolia <pavel.liolia.official@gmail.com>
2025-08-06 15:34:41 +0300

    [62]: apply authorization to apartments controller

--- Bookify.Api/Controllers/Apartments/ApartmentsController.cs
+++ Bookify.Api/Controllers/Apartments/ApartmentsController.cs
@@ -1,10 +1,11 @@
 using Bookify.Application.Apartments.SearchApartments;
-using Bookify.Domain.Abstractions;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bookify.Api.Controllers.Apartments;
 
+[Authorize]
 [ApiController]
 [Route("api/apartments")]
 public class ApartmentsController(
